cb214b0373f71ca1a556ae0c761c4d80f61d39c9
Hamidreza Montaseri <hr.montaseri@gandom.co>
2020-08-30 19:52:48 +0430

    Add player movement

--- src/Board.js
+++ src/Board.js
@@ -12,26 +12,27 @@ class Board extends React.Component {
       width: 5,
       height: 5,
       mapEntities: [],
-      map: []
+      map: [],
+      players: [],
     }
   }
 
   createMap = () => {
     let map = [];
-    const { width, height, mapEntities } = this.state;
+    const { width, height, mapEntities, players } = this.state;
     for (let i = 0; i < width; i++) {
       let row = [];
       for (let j = 0; j < height; j++) {
-        row.push({ type: "E", color: "red" });
+        row.push({ type: " ", color: "cyan" });
       }
       map.push(row);
     }
-    mapEntities.forEach(({ x, y, type }) => { map[x][y] = { type, color: "blue" } });
-    this.setState({ map });
+    mapEntities.forEach(({ x, y, type }) => { map[x][y] = { type, color: "green" } });
+    players.forEach(({ playerName, x, y }) => map[x][y] = { type: playerName, color: "red" });
+    return map;
   }
 
   componentDidMount() {
-    this.createMap();
     axios.get(urls.mapDataUrl, {
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('token')
@@ -56,21 +57,32 @@ class Board extends React.Component {
       heartbeatIncoming: 5000,
       heartbeatOutgoing: 5000,
       debug: (text) => console.log(text),
-      onConnect: this.refreshGroups,
+      onConnect: frame => {
+        console.log('Connected: ' + frame);
+        this.createdClient.subscribe('/user/queue/team', messageOutput => {
+          this.handlePlayerLoc(JSON.parse(messageOutput.body));
+        });
+      },
       onDisconnect: this.setState({ subscribed: false }),
       // onWebSocketClose,
     });
     this.createdClient.activate();
-    // var socket = new SockJS(urls.socketUrl);
-    // var username = localStorage.getItem('username');
-    // this.stompClient = Stomp.over(socket);
-    // this.stompClient.connect({}, username, frame => {
-    //   console.log('Connected: ' + frame);
-    //   this.stompClient.subscribe('/user/queue/team', messageOutput => {
-    //     console.log(JSON.parse(messageOutput.body));
-    //   });
-    // });
   };
+
+  componentWillUnmount() {
+    this.createdClient.deactivate();
+  }
+
+  handlePlayerLoc = newPlayer => {
+    console.log(newPlayer);
+    const { players } = this.state;
+    let newPlayers = [newPlayer];
+    players.forEach(player => {
+      if (player.playerName !== newPlayer.playerName)
+        newPlayers.push(player);
+    });
+    this.setState({ players: newPlayers })
+  }
 
   moveTo = (x, y) => {
     axios.post(urls.moveUrl,
@@ -83,14 +95,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const { map } = this.state
+    const map = this.createMap();
     return (
       <table>
         {
           map.map((row, index) => (
             <tr key={index}>
               {row.map(({ type, color }, jndex) =>
-                (<th key={jndex} onClick={() => this.moveTo(index, jndex)} style={{ background: color, width: "50px", height: '50px' }}>{type}</th>))}
+                (<th key={jndex} onClick={() => this.moveTo(index, jndex)} style={{ background: color, width: "50px", height: '50px' }}>{type[0]}</th>))}
             </tr>
           ))
         }
